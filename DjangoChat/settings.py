1ebc8adfd47b299b9665ad893a7efc8b62b3f5de
Vivek Yadav <vivekyadavboss2004@gmail.com>
2026-03-27 06:06:10 +0530

    changes

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chatix',
     'channels',
-    'cloudinary_storage',
-    'cloudinary',
 ]
+
+# Only add Cloudinary apps if credentials are configured (production)
+if os.environ.get('CLOUDINARY_CLOUD_NAME'):
+    INSTALLED_APPS += ['cloudinary_storage', 'cloudinary']
 
 # ===============================
 # MIDDLEWARE  (IMPORTANT ORDER)
